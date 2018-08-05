583e0835726b8e220c58f1a84f1c29c32399e8a1
Michał Maćkowiak <michal.mackowiak@netguru.pl>
2018-08-05 20:35:11 +0200

    Fix repository view test file

--- test/libguru_web/views/repository_view_test.exs
+++ test/libguru_web/views/repository_view_test.exs
@@ -1,9 +1,3 @@
 defmodule LibguruWeb.RepositoryViewTest do
   use LibguruWeb.ConnCase, async: true
-
-  alias LibguruWeb.{Repository, Repo}
-
-  def repositories
-    Repo.all(Repository)
-  end
 end
