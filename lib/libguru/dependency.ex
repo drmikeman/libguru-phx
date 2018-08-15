1003f627ef4b177ae31b6560de663a64b0e3f478
Michał Maćkowiak <michal.mackowiak@netguru.pl>
2018-08-15 15:10:21 +0200

    Remove primary key from Dependency

--- lib/libguru/dependency.ex
+++ lib/libguru/dependency.ex
@@ -2,6 +2,7 @@ defmodule Libguru.Dependency do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key false
   schema "dependencies" do
     belongs_to :library, Libguru.Library
     belongs_to :repository, Libguru.Repository
@@ -13,5 +14,6 @@ defmodule Libguru.Dependency do
     dependency
     |> cast(attrs, [:library_id, :repository_id])
     |> validate_required([:library_id, :repository_id])
+    |> unique_constraint(:library_id, name: "libraries_repositories_library_id_repository_id_index")
   end
 end
--- priv/repo/migrations/20180809192418_create_dependencies.exs
+++ priv/repo/migrations/20180809192418_create_dependencies.exs
@@ -2,7 +2,7 @@ defmodule Libguru.Repo.Migrations.CreateDependencies do
   use Ecto.Migration
 
   def change do
-    create table(:dependencies) do
+    create table(:dependencies, primary_key: false) do
       add :library_id, references(:libraries, on_delete: :nothing)
       add :repository_id, references(:repositories, on_delete: :nothing)
 
